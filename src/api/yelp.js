d32f8c25c7bbf4bf11ea26e326b8aee39b6940cd
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-08-28 01:53:33 -0700

    finished 9

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer_r7sOwzo3qW-aSu6sDMlMRw_U2XG8veDjTcBfmKzyA_-EmLqRDrIq_2YaHcVwcEnCMs9pq-ungahty3lpWBY4Kii2lsLmxMOsEfZf4-cYp6p9nHfbKeEOMwBTkcpYXYx",
+      "Bearer _r7sOwzo3qW-aSu6sDMlMRw_U2XG8veDjTcBfmKzyA_-EmLqRDrIq_2YaHcVwcEnCMs9pq-ungahty3lpWBY4Kii2lsLmxMOsEfZf4-cYp6p9nHfbKeEOMwBTkcpYXYx",
   },
 });
